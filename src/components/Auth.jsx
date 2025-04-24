1e3afca2729ddddb3ed77f68b7d3903480549571
Arnold <arnoldkorir201@gmail.com>
2025-04-24 13:47:51 +0300

    return new errors and constaruct a condition statement to proceed to the login or dispaly errors

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -21,4 +21,23 @@ const Auth = ({ onLogin, onSignup }) => {
     consent: false,
     terms: false,
   });
+
+  //validation rules
+
+  const validateLogin = () => {
+    const newErrors = {};
+
+    if (!validator.isEmail(loginForm.email)) {
+      newErrors.email = "plaese enter a valid email address";
+    }
+    if (loginForm.password.length < 8) {
+      newErrors.password = "password must be atleast 8 characters";
+    }
+    const errors = validateLogin();
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors); // show errors in the UI
+    } else {
+      onLogin(loginForm); // proceed to login
+    }
+  };
 };
