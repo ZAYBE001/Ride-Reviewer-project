f779d371ec496c1f2a38600f934cf62a9c8f647f
Arnold <arnoldkorir201@gmail.com>
2025-04-24 13:21:01 +0300

    track whether the user is in the login or signup form and incooperate a error&& auth request in progress  handling phenomena from the server side

---
+++ src/components/Auth.jsx
@@ -0,0 +1,13 @@
+//this Auth will handle both login and sign up
+import React, { useState } from "react";
+
+const Auth = ({ onLogin, onSignup }) => {
+  const [islogin, setIsLogin] = useState(true);
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const [loginForm, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+};
